39db0f14ea70373be6673b19e2c348392bcd9c94
rushi.jagani <rushi.jagani@gmail.com>
2020-07-23 18:44:00 +0530

    Gitbub info updated

--- src/container/Content/index.js
+++ src/container/Content/index.js
@@ -8,12 +8,37 @@ import './content.scss';
 const Content = () => {
   return (
     <div className="content content__container">
-      <h2>Hi there, I'm Rushi!</h2>
+      <h2>
+        Hi there <span className="icons">👋</span>, I'm Rushi!
+      </h2>
       <span className="content__role">Front End Specialist</span>
+      <h3>About Me:</h3>{' '}
       <p>
-        I like making fun, interactive things with code. I mostly work with UI
-        technologies HTML, CSS & JavaScript (React.js), but have also experience
-        with PHP (WordPress).
+        I am extremely passionate about developing performant{' '}
+        <span className="icons">🚀</span>, user-focused UIs{' '}
+        <span className="icons"></span>😍 web applications{' '}
+        <span className="icons">💻</span> with the best and latest technology. I
+        love
+        <span className="icons">❤️</span>️ working with ReactJS{' '}
+        <span className="icons">⚛️</span>, HTML, CSS and JavaScript but I also
+        spend most of the time exploring the latest technologies like NodeJS,
+        GatsbyJS, NextJS, GraphQL. I also worked with PHP, WordPress
+        Theme/Plugin development.
+      </p>
+      <h3>Personally:</h3>{' '}
+      <p>
+        I'm very fond of music <span className="icons">🎼</span> of various
+        genres. Apart from that the mountains are my favorite terrain, and I
+        live in the Pune(IN) city which becomes hill station in rainy season due
+        to an extensive mountain range. Rainy season turns the entire dry lands
+        into beautiful nature landscapes.
+      </p>
+      <h3>Staying fit:</h3>{' '}
+      <p>
+        Staying fit is important for good health, and maintaining a healthy
+        lifestyle is the key to staying fit. I start my day with a workout. I'm
+        the kind of person who really loves most exercise, but{' '}
+        <span className="icons">🚶</span> walking my favorite.
       </p>
       <div className="content__skills-wrap">
         <FrontEnd />
